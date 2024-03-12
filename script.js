f4e40d1da74a589ff72399fbf6acb6838f981908
Pascal Mielke <p.mielke87@gmx.de>
2024-03-12 22:14:14 +0100

    refactor

--- script.js
+++ script.js
@@ -57,47 +57,65 @@ let posts = [
     }
 ];
 
-const createPost = (post) => {
-    const mainSection = document.getElementById('main-section');
-    mainSection.innerHTML += /*html*/`
+const createAuthorTemplate = (post) => {
+    return /*html*/`
+        <div class="post-author">
+            <div class="author">
+                <img class="author-img" src="${post.authorImg}" alt="author">
+                <p class="author-name">${post.author}</p>
+            </div>
+            <img class="more" src="img/dots_v.png">
+        </div>
+    `;
+};
+
+const createActionsTemplate = (post) => {
+    return /*html*/`
+        <div class="actions">
+            <div class="left-actions">
+                <img class="like-img" id="like-img-${post.id}" src="img/heart.png" onclick="likePost(${post.id})">
+                <img src="img/talk.png" alt="" class="talk-img">
+                <img src="img/send.png" alt="" class="send-img">
+            </div>
+            <div class="right-actions">
+                <img src="img/bookmark.png" alt="" class="bookmark">
+            </div>
+        </div>
+    `;
+};
+
+const createCommentsSectionTemplate = (post) => {
+    return /*html*/`
+        <div class="comments" id="comments-${post.id}"></div>
+        <div class="comment-input-section">
+            <input id="comment-${post.id}" type="text" class="comment-input" placeholder="Add a comment...">
+            <button class="comment-btn" onclick="saveComment(${post.id})">Send</button>
+        </div>
+    `;
+};
+
+const createPostTemplate = (post) => {
+    return /*html*/`
         <article class="post">
-            <div class="post-author">
-                <div class="author">
-                    <img class="author-img" src="${post.authorImg}" alt="author">
-                    <p class="author-name">${post.author}</p>
-                </div>
-                <img class="more" src="img/dots_v.png">
-            </div>
+            ${createAuthorTemplate(post)}
             <img src="${post.img}" alt="" class="post-img">
-            <div class="actions">
-                <div class="left-actions">
-                    <img class="like-img" id="like-img-${post.id}" src="img/heart.png" onclick="likePost(${post.id})">
-                    <img src="img/talk.png" alt="" class="talk-img">
-                    <img src="img/send.png" alt="" class="send-img">
-                </div>
-                <div class="right-actions">
-                    <img src="img/bookmark.png" alt="" class="bookmark">
-                </div>
-            </div>
+            ${createActionsTemplate(post)}
             <p class="likes" id="likes-${post.id}">${post.likes} likes</p>
-            <div class="comments" id="comments-${post.id}"></div>
-            <div class="comment-input-section">
-                <input id="comment-${post.id}" type="text" class="comment-input" placeholder="Add a comment...">
-                <button class="comment-btn" onclick="saveComment(${post.id})">Send</button>
-            </div>
+            ${createCommentsSectionTemplate(post)}
         </article>
     `;
+};
+
+const createPost = (post) => {
+    const mainSection = document.getElementById('main-section');
+    mainSection.innerHTML += createPostTemplate(post);
     createComments(post.comments, post.id);
     likeImage(post.id);
 };
 
 const likeImage = (id) => {
     let img = document.getElementById('like-img-'+ id);
-    if(posts[id].liked){
-        img.src = 'img/red_heart.png';
-    } else {
-        img.src = 'img/heart.png';
-    }
+    img.src = posts[id].liked ? 'img/red_heart.png' : 'img/heart.png';
 };
 
 const createComments = (comments, id) => {
@@ -115,11 +133,9 @@ const createComments = (comments, id) => {
 const saveComment = (id) => {
     let comment = document.getElementById('comment-'+ id).value;
     let commentsSection = document.getElementById('comments-'+ id);
-
     if (comment === '') {
         return;
     }
-    
     commentsSection.innerHTML += /*html*/`
         <div class="comment">
             <p class="author-name"><b>You</b></p>
@@ -135,7 +151,7 @@ const saveComment = (id) => {
 };
 
 const likePost = (id) => {
-    if(posts[id].liked){
+    if (posts[id].liked) {
         posts[id].liked = false;
         document.getElementById('like-img-'+ id).src = 'img/heart.png';
         posts[id].likes--;
@@ -155,7 +171,7 @@ const createPosts = () => {
 };
 
 const load = () => {
-    if(localStorage.getItem('posts')){
+    if (localStorage.getItem('posts')) {
         posts = JSON.parse(localStorage.getItem('posts'));
     }
     createPosts();

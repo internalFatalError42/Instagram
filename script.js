1bda9d5eefa9fb4d5ba5374577033655118bc3e3
Pascal Mielke <p.mielke87@gmx.de>
2024-03-12 21:47:25 +0100

    feinschliff

--- script.js
+++ script.js
@@ -62,22 +62,42 @@ const createPost = (post) => {
     mainSection.innerHTML += /*html*/`
         <article class="post">
             <div class="post-author">
-                <img class="author-img" src="${post.authorImg}" alt="author">
-                <p class="author-name">${post.author}</p>
+                <div class="author">
+                    <img class="author-img" src="${post.authorImg}" alt="author">
+                    <p class="author-name">${post.author}</p>
+                </div>
+                <img class="more" src="img/dots_v.png">
             </div>
             <img src="${post.img}" alt="" class="post-img">
             <div class="actions">
-                <img class="like-img" id="like-img-${post.id}" src="img/heart.png" onclick="likePost(${post.id})">
-                <img src="img/talk.png" alt="" class="talk-img">
-                <img src="img/send.png" alt="" class="send-img">
+                <div class="left-actions">
+                    <img class="like-img" id="like-img-${post.id}" src="img/heart.png" onclick="likePost(${post.id})">
+                    <img src="img/talk.png" alt="" class="talk-img">
+                    <img src="img/send.png" alt="" class="send-img">
+                </div>
+                <div class="right-actions">
+                    <img src="img/bookmark.png" alt="" class="bookmark">
+                </div>
             </div>
             <p class="likes" id="likes-${post.id}">${post.likes} likes</p>
             <div class="comments" id="comments-${post.id}"></div>
-            <input id="comment-${post.id}" type="text" class="comment-input" placeholder="Add a comment...">
-            <button class="comment-btn" onclick="saveComment(${post.id})">Send</button>
+            <div class="comment-input-section">
+                <input id="comment-${post.id}" type="text" class="comment-input" placeholder="Add a comment...">
+                <button class="comment-btn" onclick="saveComment(${post.id})">Send</button>
+            </div>
         </article>
     `;
     createComments(post.comments, post.id);
+    likeImage(post.id);
+};
+
+const likeImage = (id) => {
+    let img = document.getElementById('like-img-'+ id);
+    if(posts[id].liked){
+        img.src = 'img/red_heart.png';
+    } else {
+        img.src = 'img/heart.png';
+    }
 };
 
 const createComments = (comments, id) => {
@@ -95,6 +115,11 @@ const createComments = (comments, id) => {
 const saveComment = (id) => {
     let comment = document.getElementById('comment-'+ id).value;
     let commentsSection = document.getElementById('comments-'+ id);
+
+    if (comment === '') {
+        return;
+    }
+    
     commentsSection.innerHTML += /*html*/`
         <div class="comment">
             <p class="author-name"><b>You</b></p>
